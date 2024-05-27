bf171174c8b6f62f0cba31102597290081271bc5
Samuel Levy <hi@ssh.contact>
2024-05-27 19:35:20 -0400

    i added random slug generation. yay!

--- main.go
+++ main.go
@@ -4,15 +4,28 @@
 package main
 
 import (
+	"encoding/base32"
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 )
 
 var redirects = map[string]string{
 	"ssh": "https://ssh.contact/",
+}
+
+var crock32 = base32.NewEncoding("0123456789abcdefghjkmnpqrstvwxyz").
+	WithPadding(base32.NoPadding)
+
+func generateRandomSlug(length int) (string, error) {
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return "", fmt.Errorf("argon2: failed to generate random bytes: %w", err)
+	}
+	return crock32.EncodeToString(b), nil
 }
 
 func getDestination(slug string) (string, bool) {
@@ -30,12 +43,16 @@ func handleAddRedirect(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Request body too long", http.StatusRequestEntityTooLarge)
 		return
 	}
-	if len(r.URL.Path) < 1 {
-		http.Error(w, "No slug provided", http.StatusBadRequest)
-		return
-		//TODO: Add random slug generation
-	} //put slug assignment in an else block once rand gen is done
-	slug := r.URL.Path[1:]
+	var slug string
+	if len(r.URL.Path) < 2 {
+		slug, err = generateRandomSlug(5)
+		if err != nil {
+			http.Error(w, "Failed to generate randomized slug and none provided", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		slug = r.URL.Path[1:]
+	}
 
 	dest := string(body)
 	err = addDestination(slug, dest)
@@ -43,7 +60,7 @@ func handleAddRedirect(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unknown server error in addition of redirect mapping to store", http.StatusInternalServerError)
 		return
 	}
-
+	fmt.Fprintf(w, "/%s", slug)
 }
 
 func handleRedirect(w http.ResponseWriter, r *http.Request) {
